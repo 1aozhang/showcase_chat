777fada9ad9e99130deceab46d4471e1ffcac8a4
1aohang <118659034+1aozhang@users.noreply.github.com>
2023-02-11 20:11:19 +0800

    Update Page.tsx

--- helpers/Page.tsx
+++ helpers/Page.tsx
@@ -11,7 +11,6 @@ export function Page({ children }: { children: ComponentChildren }) {
       />
       <div class="flex flex-col justify-center items-center w-full h-screen children:(bg-[#F9F9F9] border-1 border-gray-300)">
         {children}
-        <Footer />
       </div>
     </>
   );
