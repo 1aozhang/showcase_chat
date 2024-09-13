0965431a96f79553bbbafcea07a1a29294694de0
1aohang <118659034+1aozhang@users.noreply.github.com>
2024-09-13 21:06:26 +0800

    更新 database.ts
    ssl ==> false

--- communication/database.ts
+++ communication/database.ts
@@ -153,7 +153,8 @@ export const databaseLoader = new ResourceLoader<Database>({
     );
     const sql = postgres(getEnvOrThrow("SUPABASE_POSTGRES_URI"), {
       keep_alive: false, // Otherwise required '--unstable' flag.
-      ssl: { caCerts: [caCert] },
+      //ssl: { caCerts: [caCert] },
+      ssl: false
     });
     await sql`
       create table if not exists users (
